978992b80cc4bca8f04c5c113675f9d881705e64
Maxi Combina <maxicombina@gmail.com>
2025-11-08 18:31:56 +0100

    Fix in handling response

--- strava_integration/management/commands/detect_missing_activities.py
+++ strava_integration/management/commands/detect_missing_activities.py
@@ -1,26 +1,20 @@
 # strava_integration/management/commands/detect_missing_activities.py
 from django.core.management.base import BaseCommand
 from strava_integration.services import detect_and_save_missing_activities
-import json
 
 class Command(BaseCommand):
     help = "Detect missing Strava activities, store them in MissingActivity, and show a summary."
 
     def handle(self, *args, **options):
-        response = detect_and_save_missing_activities()
+
         try:
-            data = json.loads(response)
+            response = detect_and_save_missing_activities()
         except Exception:
-            self.stdout.write(self.style.ERROR("Could not parse response JSON."))
-            self.stdout.write(str(response))
+            self.stdout.write(self.style.ERROR("Error detecting and saveing missing activities"))
             return
 
-        if data.get("status") == "ok":
-            self.stdout.write(self.style.SUCCESS("✅ Missing activities detected successfully."))
-            self.stdout.write(f"Total missing detected: {data['total_missing_detected']}")
-            self.stdout.write(f"New missing added: {data['new_missing_added']}")
-            self.stdout.write(f"Already present (unloaded): {data['already_present_unloaded']}")
-            self.stdout.write(f"Already present (loaded): {data['already_present_loaded']}")
-        else:
-            self.stdout.write(self.style.ERROR("❌ Error detecting missing activities"))
-            self.stdout.write(data.get("message", "Unknown error"))
+        self.stdout.write(self.style.SUCCESS("✅ Missing activities detected successfully."))
+        self.stdout.write(f"Total missing detected: {response['total_missing_detected']}")
+        self.stdout.write(f"New missing added: {response['new_missing_added']}")
+        self.stdout.write(f"Already present (unloaded): {response['already_present_unloaded']}")
+        self.stdout.write(f"Already present (loaded): {response['already_present_loaded']}")

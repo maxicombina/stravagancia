ccbdee35da6c40b8e97f50b613c601a3c7b593ff
Maxi Combina <maxicombina@gmail.com>
2026-04-13 23:06:19 +0200

    Create superuser automatically

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,19 @@ set -e
 echo "Running migrations..."
 uv run python manage.py migrate --noinput
 
+echo "Creating superuser if it doesn't exist..."
+uv run python manage.py shell -c "
+from django.contrib.auth.models import User
+import os
+username = os.environ.get('DJANGO_SUPERUSER_USERNAME', 'admin')
+password = os.environ.get('DJANGO_SUPERUSER_PASSWORD', 'admin')
+email = os.environ.get('DJANGO_SUPERUSER_EMAIL', '')
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username, email, password)
+    print(f'Superuser {username!r} created.')
+else:
+    print(f'Superuser {username!r} already exists, skipping.')
+"
+
 echo "Starting Django..."
 exec uv run python manage.py runserver 0.0.0.0:8000
